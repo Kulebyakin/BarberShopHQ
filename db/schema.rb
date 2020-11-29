c513723678eac8141fdc38ec6b9fb3dbea53a87d
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-11-29 17:59:15 +0800

    Barber migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_091726) do
+ActiveRecord::Schema.define(version: 2020_11_29_095235) do
+
+  create_table "barber", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "client", force: :cascade do |t|
     t.text "name"
